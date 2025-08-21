edab596ac35507437c0e7560cebeefef928ace36
Alpha Diack <alpha.diack@intouchgroup.net>
2025-08-21 13:03:45 +0000

    bug correction crashlytics

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -177,7 +177,7 @@ class _AppState extends State<App> {
 
   @override
   Widget build(BuildContext context) {
-    WidgetsFlutterBinding.ensureInitialized();
+    //WidgetsFlutterBinding.ensureInitialized();
 
     // Afficher la page de chargement
     if (_isLoading) {

7f35ee1bd623b7f3ed982c0c8aee23a2f1463e2d
Issa Ba <issa.ba@intouchgroup.net>
2025-08-29 11:55:39 +0000

    correction guID

--- lib/modules/transactions/domain/models/mappers/movement_mappers.dart
+++ lib/modules/transactions/domain/models/mappers/movement_mappers.dart
@@ -44,6 +44,7 @@ extension MovementDetailsMapper on MovementDetailsDTO {
       facture: null,
       dateExpiration: null,
       txId: guID,
+      guID: guID,
       dateDebut: null,
       dateFin: null,
       frequence: null,

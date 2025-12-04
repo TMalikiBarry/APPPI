ff5064cefb93ee02cb27b6c14eea320a97de0cb8
Issa Ba <issa.ba@intouchgroup.net>
2025-12-04 18:27:38 +0000

    Deactivation of the delete button for the CI

--- lib/modules/profile/presentation/pages/compte/profile_compte_details_page.dart
+++ lib/modules/profile/presentation/pages/compte/profile_compte_details_page.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../../../shared/widgets/my_page_container.dart';
 import '../../../../alias/presentation/pages/alias_delete_btn_widget.dart';
 import '../../../../compte/presentation/pages/compte_details_widget.dart';
 
-class ProfileCompteDetailsPage extends StatelessWidget {
+class ProfileCompteDetailsPage extends StatefulWidget {
   ///
   const ProfileCompteDetailsPage({super.key});
+
+  @override
+  State<ProfileCompteDetailsPage> createState() => _ProfileCompteDetailsPageState();
+}
+
+class _ProfileCompteDetailsPageState extends State<ProfileCompteDetailsPage> {
+  String? whichCountry;
+  bool isLoading = true;
+
+  init() async {
+    var pref = await SharedPreferences.getInstance();
+    whichCountry = pref.getString('countryCode') ?? "SN";
+    print("whichCountry : $whichCountry");
+    if (mounted) {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    init();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +45,9 @@ class ProfileCompteDetailsPage extends StatelessWidget {
       ),
 
       // Boutton supprimer alias
-      bottomNavigationBar: const AliasDeleteBtnWidget(),
+      bottomNavigationBar: isLoading ? null : whichCountry == "CI"
+        ? null
+        : const AliasDeleteBtnWidget(),
     );
   }
 }
